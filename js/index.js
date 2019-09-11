d2f90f86e93616ef6e3440b715a61e683748a914
Tony Walker <Walkertony509@gmail.com>
2019-09-10 20:26:50 -0700

    I added an event that changes the logo styling.

--- js/index.js
+++ js/index.js
@@ -12,3 +12,11 @@ const bodyStyle = document.querySelector('body');
 bodyStyle.addEventListener('wheel', function(e){
     e.target.style.background= 'lightBlue';
 });
+
+// Logo
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('dblclick', function(e){
+    e.target.style.color='lightblue';
+    e.target.style.fontWeight ='bold';
+    e.target.style.fontSize= '80px';
+});
