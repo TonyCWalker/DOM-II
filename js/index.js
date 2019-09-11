6a1187f598bf81525ae6bb651b1afae60aec6789
Tony Walker <Walkertony509@gmail.com>
2019-09-10 20:24:52 -0700

    I added an event that changes the main navigation color.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// Main-navigation
+const navBackground = document.querySelector('.main-navigation');
+navBackground.addEventListener('mouseover', function(e){
+    e.target.style.background = 'yellow';
+    e.preventDefault();
+});
