54c3c10d37d851f9a9eebb55ba6ebd9d52bce477
Tony Walker <Walkertony509@gmail.com>
2019-09-10 20:25:59 -0700

    I added an event that changes the background color of the body section.

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ navBackground.addEventListener('mouseover', function(e){
     e.target.style.background = 'yellow';
     e.preventDefault();
 });
+
+// Body
+const bodyStyle = document.querySelector('body');
+bodyStyle.addEventListener('wheel', function(e){
+    e.target.style.background= 'lightBlue';
+});
